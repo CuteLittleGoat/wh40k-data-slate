998e849f3bacad63e226388cf4bab80d2baaed5d
CuteLittleGoat <taryf@o2.pl>
2025-12-13 09:10:34 +0100

    Create firebase-config.template.js

---
+++ config/firebase-config.template.js
@@ -0,0 +1,12 @@
+// Skopiuj ten plik jako: config/firebase-config.js (bez ".template")
+// Następnie wklej swój firebaseConfig z:
+// Firebase Console → Project settings → Your apps (Web) → Firebase SDK snippet (Config)
+
+export const firebaseConfig = {
+  apiKey: "PASTE_HERE",
+  authDomain: "PASTE_HERE",
+  projectId: "PASTE_HERE",
+  storageBucket: "PASTE_HERE",
+  messagingSenderId: "PASTE_HERE",
+  appId: "PASTE_HERE"
+};
