ebd2e824dff1cc263b044091fa5f9f5dd9f0435f
CuteLittleGoat <taryf@o2.pl>
2025-12-13 09:56:41 +0100

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,8 +1,12 @@
-export const firebaseConfig = {
+// config/firebase-config.js
+// GLOBALNA konfiguracja Firebase dla GM.html i Infoczytnik.html
+// (nie używamy "export", żeby działało też z firebase-*-compat)
+
+window.firebaseConfig = {
   apiKey: "AIzaSyCVoWzXtO-vipsxnvZlFkcqcNgYYuH3osc",
   authDomain: "wh40k-data-slate.firebaseapp.com",
   projectId: "wh40k-data-slate",
   storageBucket: "wh40k-data-slate.firebasestorage.app",
   messagingSenderId: "382792444120",
-  appId: "1:382792444120:web:9eb27e2ed29109ac838fad"
+  appId: "1:382792444120:web:9eb27e2ed29109ac838fad",
 };
